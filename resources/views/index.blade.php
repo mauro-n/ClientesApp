b31dd9be237a9b1d1570258f394330a061916183
test <mauro.henrique613@gmail.com>
2023-09-01 15:56:01 -0300

    Front end numeric keyboard on mobile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <!-- Google Fonts -->
-    <script type="module" crossorigin src="{{ asset('/assets/index-fd730735.js') }}"></script>
+    <script type="module" crossorigin src="{{ asset('/assets/index-dfbe0125.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('/assets/index-a8773972.css') }}">
   </head>
   <body>
