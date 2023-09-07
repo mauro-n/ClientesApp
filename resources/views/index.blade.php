078b4a9fc3674b7c08d5f791f902da6c2624569b
test <mauro.henrique613@gmail.com>
2023-09-07 15:52:10 -0300

    Complete frontend remake

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <!-- Google Fonts -->
-    <script type="module" crossorigin src="{{ asset('/assets/index-1eb97760.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('/assets/index-b44850c7.css') }}">
+    <script type="module" crossorigin src="{{ asset('/assets/index-8fc5dc6f.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('/assets/index-9b4e157d.css') }}">
   </head>
   <body>
     <div id="root"></div>
