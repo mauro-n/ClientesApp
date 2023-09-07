50d6e23674470a79fff48ea5a427a375be2ef92b
test <mauro.henrique613@gmail.com>
2023-09-07 15:55:10 -0300

    Fix api url

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <!-- Google Fonts -->
-    <script type="module" crossorigin src="{{ asset('/assets/index-8fc5dc6f.js') }}"></script>
+    <script type="module" crossorigin src="{{ asset('/assets/index-cf7f58f0.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('/assets/index-9b4e157d.css') }}">
   </head>
   <body>
