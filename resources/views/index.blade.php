b989682cde34e97b128f6fbb47b2863f84251b2c
test <mauro.henrique613@gmail.com>
2023-09-07 16:12:33 -0300

    Fix padding on add transaction page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <!-- Google Fonts -->
-    <script type="module" crossorigin src="{{ asset('/assets/index-cf7f58f0.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('/assets/index-9b4e157d.css') }}">
+    <script type="module" crossorigin src="{{ asset('/assets/index-de30460a.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('/assets/index-39494c03.css') }}">
   </head>
   <body>
     <div id="root"></div>
