5f53c415ead816cb3663b5402053a9a239806b1d
test <mauro.henrique613@gmail.com>
2023-09-10 17:36:55 -0300

    Major frontend refactor

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <!-- Google Fonts -->
-    <script type="module" crossorigin src="{{ asset('/assets/index-46531da1.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('/assets/index-39494c03.css') }}">
+    <script type="module" crossorigin src="{{ asset('/assets/index-1526ad0a.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('/assets/index-0ee3e565.css') }}">
   </head>
   <body>
     <div id="root"></div>
